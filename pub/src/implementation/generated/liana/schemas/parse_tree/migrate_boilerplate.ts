a6e77d4df9dd1d3a7dc3d6fb24d5397c7d933b82
Corno Schraverus <corno@schraverus.com>
2026-01-23 09:57:59 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/parse_tree/migrate_boilerplate.ts
@@ -0,0 +1,180 @@
+
+import * as _p from "pareto-core/dist/transformer"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/parse_tree/migrate_boilerplate"
+
+import * as t_out from "../../../../../interface/generated/liana/schemas/parse_tree/data"
+
+import * as v_token from "../token/migrate_boilerplate"
+export const Structural_Token: t_signatures.Structural_Token = ($) => ({
+    'trailing trivia': _p.deprecated_cc($['trailing trivia'], ($) => v_token.Trivia(
+        $
+    )),
+    'range': _p.deprecated_cc($['range'], ($) => v_token.Range(
+        $
+    )),
+})
+export const Text: t_signatures.Text = ($) => ({
+    'trailing trivia': _p.deprecated_cc($['trailing trivia'], ($) => v_token.Trivia(
+        $
+    )),
+    'range': _p.deprecated_cc($['range'], ($) => v_token.Range(
+        $
+    )),
+    'value': _p.deprecated_cc($['value'], ($) => $),
+    'type': _p.deprecated_cc($['type'], ($) => v_token.Text_Type(
+        $
+    )),
+})
+export const Key_Value_Pairs: t_signatures.Key_Value_Pairs = ($) => $.__l_map(($) => ({
+    'key': _p.deprecated_cc($['key'], ($) => Text(
+        $
+    )),
+    'value': _p.deprecated_cc($['value'], ($) => $.__o_map(($) => ({
+        ':': _p.deprecated_cc($[':'], ($) => Structural_Token(
+            $
+        )),
+        'value': _p.deprecated_cc($['value'], ($) => Value(
+            $
+        )),
+    }))),
+}))
+export const Elements: t_signatures.Elements = ($) => $.__l_map(($) => ({
+    'value': _p.deprecated_cc($['value'], ($) => Value(
+        $
+    )),
+}))
+export const Value: t_signatures.Value = ($) => ({
+    'type': _p.deprecated_cc($['type'], ($) => _p.deprecated_cc($, ($): t_out.Value.type_ => {
+        switch ($[0]) {
+            case 'concrete': return _p.ss($, ($) => ['concrete', _p.deprecated_cc($, ($): t_out.Value.type_.concrete => {
+                switch ($[0]) {
+                    case 'dictionary': return _p.ss($, ($) => ['dictionary', ({
+                        '{': _p.deprecated_cc($['{'], ($) => Structural_Token(
+                            $
+                        )),
+                        'entries': _p.deprecated_cc($['entries'], ($) => Key_Value_Pairs(
+                            $
+                        )),
+                        '}': _p.deprecated_cc($['}'], ($) => Structural_Token(
+                            $
+                        )),
+                    })])
+                    case 'group': return _p.ss($, ($) => ['group', _p.deprecated_cc($, ($): t_out.Value.type_.concrete.group => {
+                        switch ($[0]) {
+                            case 'concise': return _p.ss($, ($) => ['concise', ({
+                                '<': _p.deprecated_cc($['<'], ($) => Structural_Token(
+                                    $
+                                )),
+                                'elements': _p.deprecated_cc($['elements'], ($) => Elements(
+                                    $
+                                )),
+                                '>': _p.deprecated_cc($['>'], ($) => Structural_Token(
+                                    $
+                                )),
+                            })])
+                            case 'verbose': return _p.ss($, ($) => ['verbose', ({
+                                '(': _p.deprecated_cc($['('], ($) => Structural_Token(
+                                    $
+                                )),
+                                'entries': _p.deprecated_cc($['entries'], ($) => Key_Value_Pairs(
+                                    $
+                                )),
+                                ')': _p.deprecated_cc($[')'], ($) => Structural_Token(
+                                    $
+                                )),
+                            })])
+                            default: return _p.au($[0])
+                        }
+                    })])
+                    case 'list': return _p.ss($, ($) => ['list', ({
+                        '[': _p.deprecated_cc($['['], ($) => Structural_Token(
+                            $
+                        )),
+                        'elements': _p.deprecated_cc($['elements'], ($) => Elements(
+                            $
+                        )),
+                        ']': _p.deprecated_cc($[']'], ($) => Structural_Token(
+                            $
+                        )),
+                    })])
+                    case 'nothing': return _p.ss($, ($) => ['nothing', ({
+                        '~': _p.deprecated_cc($['~'], ($) => Structural_Token(
+                            $
+                        )),
+                    })])
+                    case 'optional': return _p.ss($, ($) => ['optional', _p.deprecated_cc($, ($): t_out.Value.type_.concrete.optional => {
+                        switch ($[0]) {
+                            case 'set': return _p.ss($, ($) => ['set', ({
+                                '*': _p.deprecated_cc($['*'], ($) => Structural_Token(
+                                    $
+                                )),
+                                'value': _p.deprecated_cc($['value'], ($) => Value(
+                                    $
+                                )),
+                            })])
+                            default: return _p.au($[0])
+                        }
+                    })])
+                    case 'state group': return _p.ss($, ($) => ['state group', ({
+                        '|': _p.deprecated_cc($['|'], ($) => Structural_Token(
+                            $
+                        )),
+                        'status': _p.deprecated_cc($['status'], ($) => _p.deprecated_cc($, ($): t_out.Value.type_.concrete.state_group.status => {
+                            switch ($[0]) {
+                                case 'missing data': return _p.ss($, ($) => ['missing data', ({
+                                    '#': _p.deprecated_cc($['#'], ($) => Structural_Token(
+                                        $
+                                    )),
+                                })])
+                                case 'set': return _p.ss($, ($) => ['set', ({
+                                    'state': _p.deprecated_cc($['state'], ($) => Text(
+                                        $
+                                    )),
+                                    'value': _p.deprecated_cc($['value'], ($) => Value(
+                                        $
+                                    )),
+                                })])
+                                default: return _p.au($[0])
+                            }
+                        })),
+                    })])
+                    case 'text': return _p.ss($, ($) => ['text', Text(
+                        $
+                    )])
+                    default: return _p.au($[0])
+                }
+            })])
+            case 'include': return _p.ss($, ($) => ['include', ({
+                '@': _p.deprecated_cc($['@'], ($) => Structural_Token(
+                    $
+                )),
+                'path': _p.deprecated_cc($['path'], ($) => Text(
+                    $
+                )),
+            })])
+            case 'missing data': return _p.ss($, ($) => ['missing data', ({
+                '#': _p.deprecated_cc($['#'], ($) => Structural_Token(
+                    $
+                )),
+            })])
+            default: return _p.au($[0])
+        }
+    })),
+})
+export const Content: t_signatures.Content = ($) => Value(
+    $
+)
+export const Document: t_signatures.Document = ($) => ({
+    'header': _p.deprecated_cc($['header'], ($) => $.__o_map(($) => ({
+        '!': _p.deprecated_cc($['!'], ($) => Structural_Token(
+            $
+        )),
+        'value': _p.deprecated_cc($['value'], ($) => Value(
+            $
+        )),
+    }))),
+    'content': _p.deprecated_cc($['content'], ($) => Content(
+        $
+    )),
+})
---
+++ pub/src/implementation/generated/liana/schemas/unmarshall_errors/migrate_boilerplate.ts
@@ -0,0 +1,86 @@
+
+import * as _p from "pareto-core/dist/transformer"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/unmarshall_errors/migrate_boilerplate"
+
+import * as t_out from "../../../../../interface/generated/liana/schemas/unmarshall_errors/data"
+export const Relative_Location: t_signatures.Relative_Location = ($) => ({
+    'line': _p.deprecated_cc($['line'], ($) => $),
+    'column': _p.deprecated_cc($['column'], ($) => $),
+})
+export const Location: t_signatures.Location = ($) => ({
+    'relative': _p.deprecated_cc($['relative'], ($) => Relative_Location(
+        $
+    )),
+    'absolute': _p.deprecated_cc($['absolute'], ($) => $),
+})
+export const Range: t_signatures.Range = ($) => ({
+    'start': _p.deprecated_cc($['start'], ($) => Location(
+        $
+    )),
+    'end': _p.deprecated_cc($['end'], ($) => Location(
+        $
+    )),
+})
+export const Errors: t_signatures.Errors = ($) => $.__l_map(($) => ({
+    'range': _p.deprecated_cc($['range'], ($) => Range(
+        $
+    )),
+    'type': _p.deprecated_cc($['type'], ($) => _p.deprecated_cc($, ($): t_out.Errors.L.type_ => {
+        switch ($[0]) {
+            case 'error': return _p.ss($, ($) => ['error', _p.deprecated_cc($, ($): t_out.Errors.L.type_.error => {
+                switch ($[0]) {
+                    case 'invalid value type': return _p.ss($, ($) => ['invalid value type', ({
+                        'expected': _p.deprecated_cc($['expected'], ($) => $.__l_map(($) => _p.deprecated_cc($, ($): t_out.Errors.L.type_.error.invalid_value_type.expected.L => {
+                            switch ($[0]) {
+                                case 'text': return _p.ss($, ($) => ['text', null])
+                                case 'verbose group': return _p.ss($, ($) => ['verbose group', null])
+                                case 'concise group': return _p.ss($, ($) => ['concise group', null])
+                                case 'dictionary': return _p.ss($, ($) => ['dictionary', null])
+                                case 'not set': return _p.ss($, ($) => ['not set', null])
+                                case 'set': return _p.ss($, ($) => ['set', null])
+                                case 'list': return _p.ss($, ($) => ['list', null])
+                                case 'state': return _p.ss($, ($) => ['state', null])
+                                default: return _p.au($[0])
+                            }
+                        }))),
+                    })])
+                    case 'duplicate property': return _p.ss($, ($) => ['duplicate property', ({
+                        'name': _p.deprecated_cc($['name'], ($) => $),
+                    })])
+                    case 'missing property': return _p.ss($, ($) => ['missing property', ({
+                        'name': _p.deprecated_cc($['name'], ($) => $),
+                    })])
+                    case 'superfluous property': return _p.ss($, ($) => ['superfluous property', ({
+                        'name': _p.deprecated_cc($['name'], ($) => $),
+                    })])
+                    case 'state': return _p.ss($, ($) => ['state', _p.deprecated_cc($, ($): t_out.Errors.L.type_.error.state => {
+                        switch ($[0]) {
+                            case 'more than 2 elements': return _p.ss($, ($) => ['more than 2 elements', null])
+                            case 'missing state name': return _p.ss($, ($) => ['missing state name', null])
+                            case 'state is not a string': return _p.ss($, ($) => ['state is not a string', null])
+                            case 'missing value': return _p.ss($, ($) => ['missing value', null])
+                            case 'unknown state': return _p.ss($, ($) => ['unknown state', ({
+                                'found': _p.deprecated_cc($['found'], ($) => $),
+                                'expected': _p.deprecated_cc($['expected'], ($) => $.__d_map(($) => null)),
+                            })])
+                            case 'missing data marker': return _p.ss($, ($) => ['missing data marker', null])
+                            default: return _p.au($[0])
+                        }
+                    })])
+                    default: return _p.au($[0])
+                }
+            })])
+            case 'warning': return _p.ss($, ($) => ['warning', _p.deprecated_cc($, ($): t_out.Errors.L.type_.warning => {
+                switch ($[0]) {
+                    case 'expected apostrophed string': return _p.ss($, ($) => ['expected apostrophed string', null])
+                    case 'expected quoted string': return _p.ss($, ($) => ['expected quoted string', null])
+                    case 'expected backticked string': return _p.ss($, ($) => ['expected backticked string', null])
+                    case 'expected undelimited string': return _p.ss($, ($) => ['expected undelimited string', null])
+                    default: return _p.au($[0])
+                }
+            })])
+            default: return _p.au($[0])
+        }
+    })),
+}))
