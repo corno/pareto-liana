5ae2ce0e108de246667a9c8f41a921bece93e70c
Corno Schraverus <corno@schraverus.com>
2026-01-23 09:59:52 +0100

    ...

--- pub/src/implementation/generated/liana/schemas/unmarshall_result/migrate_boilerplate.ts
+++ pub/src/implementation/generated/liana/schemas/unmarshall_result/migrate_boilerplate.ts
@@ -1,12 +1,12 @@
 
-import * as _p from "pareto-core/dist/transformer"
+// import * as _p from "pareto-core/dist/transformer"
 
-import * as t_signatures from "../../../../../interface/generated/liana/schemas/unmarshall_result/migrate_boilerplate"
+// import * as t_signatures from "../../../../../interface/generated/liana/schemas/unmarshall_result/migrate_boilerplate"
 
-import * as t_out from "../../../../../interface/generated/liana/schemas/unmarshall_result/data"
+// import * as t_out from "../../../../../interface/generated/liana/schemas/unmarshall_result/data"
 
-import * as v_schema from "../schema/migrate_boilerplate"
+// import * as v_schema from "../schema/migrate_boilerplate"
 
-import * as v_parse_tree from "../parse_tree/migrate_boilerplate"
+// import * as v_parse_tree from "../parse_tree/migrate_boilerplate"
 
-import * as v_token from "../token/migrate_boilerplate"
+// import * as v_token from "../token/migrate_boilerplate"
