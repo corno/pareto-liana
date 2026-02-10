ef7fdb347554e1aa11c6a282d0cb3a67dabd55fe
Corno Schraverus <corno@schraverus.com>
2026-02-10 16:41:34 +0100

    pdt: ...

--- pub/src/data/temporary_schemas/pareto-graphviz/schemas/low_level/$.types.astn.ts
+++ pub/src/data/temporary_schemas/pareto-graphviz/schemas/low_level/$.types.astn.ts
@@ -24,10 +24,11 @@ export const $: g_.Modules = modules(
             "name": prop(t.optional(t.component("ID"))),
             "statements": prop(t.component("Statement List")),
         })),
+
         "Statement List": module_(t.list(t.state({
             "node": toption(t.group({
                 "node": prop(t.component("Node ID")),
-                "attribute list": prop(t.component("Attribute List")),
+                "attributes": prop(t.component_external("attributes", "Attributes")),
             })),
             "edge": toption(t.group({
                 "left": prop(t.state({
@@ -39,7 +40,7 @@ export const $: g_.Modules = modules(
                     "node": toption(t.component("Node ID")),
                     "subgraph": toption(t.component("Subgraph")),
                 }))),
-                "attributes": prop(t.component("Attribute List")),
+                "attributes": prop(t.component_external("attributes", "Attributes")),
             })),
             "attribute list": toption(t.group({
                 "type": prop(t.state({
@@ -47,7 +48,7 @@ export const $: g_.Modules = modules(
                     "node": toption(t.nothing()),
                     "edge": toption(t.nothing()),
                 })),
-                "attributes": prop(t.component("Attribute List")),
+                "attributes": prop(t.component_external("attributes", "Attributes")),
             })),
             "attribute assignment": toption(t.group({
                 "name": prop(t.component("ID")),
@@ -55,10 +56,12 @@ export const $: g_.Modules = modules(
             })),
             "subgraph": toption(t.component("Subgraph")),
         }))),
+
         "Attribute List": module_(t.list(t.group({
             "name": prop(t.component("ID")),
             "value": prop(t.component("ID")),
         }))),
+
         "Node ID": module_(t.group({
             "id": prop(t.component("ID")),
             "port": prop(t.optional(t.group({
@@ -66,12 +69,14 @@ export const $: g_.Modules = modules(
                 "compass point": prop(t.optional(t.component("ID"))),
             })))
         })),
+
         "ID": module_(t.state({
             "id": toption(t.text_global("id")),
             "string": toption(t.text_global("Text Value")),
             "html": toption(t.text_global("Text Value")),
             "number": toption(t.number_local(n.integer(null))),
         })),
+
         "Subgraph": module_(t.group({
             "subgraph": prop(t.optional(t.optional(t.component("ID")))), // is it a subgraph? and if yes, does it have a name?
             "statements": prop(t.component("Statement List")),
