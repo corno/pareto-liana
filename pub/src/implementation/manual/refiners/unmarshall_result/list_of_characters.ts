0cebb80f672664ebaee70b19d26d3f868b40bf97
Corno Schraverus <corno@schraverus.com>
2026-03-01 12:15:34 +0100

    pdt: published version 0.0.17

--- pub/src/implementation/manual/refiners/unmarshall_result/list_of_characters.ts
+++ pub/src/implementation/manual/refiners/unmarshall_result/list_of_characters.ts
@@ -3,8 +3,6 @@ import * as _pi from 'pareto-core/dist/interface'
 import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
 
 //data types
-import * as d_parse_tree from "astn-core/dist/interface/generated/liana/schemas/parse_tree/data"
-import * as d_schema from "../../../../interface/generated/liana/schemas/schema/data/resolved"
 import * as d_out from "../../../../interface/to_be_generated/unmashall_result"
 import * as d_function from "../../../../interface/to_be_generated/deserialize_unmarshall_result"
 import * as d_in from "pareto-fountain-pen/dist/interface/generated/liana/schemas/list_of_characters/data"
---
+++ pub/src/implementation/manual/refiners/unmarshall_result/text.ts
@@ -0,0 +1,37 @@
+import * as _p from 'pareto-core/dist/assign'
+import * as _pi from 'pareto-core/dist/interface'
+import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
+
+//data types
+import * as d_out from "../../../../interface/to_be_generated/unmashall_result"
+import * as d_function from "../../../../interface/to_be_generated/deserialize_unmarshall_result"
+import * as d_in from "pareto-fountain-pen/dist/interface/generated/liana/schemas/text/data"
+
+
+type Parameters = {
+    'instance path': string
+    'schema content': string
+    'schema path': string
+    'tab size': number
+}
+
+export type Value = _pi.Refiner_With_Parameter<d_out.Value, d_function.Error, d_in.Text, Parameters>
+export type Document = _pi.Refiner_With_Parameter<d_out.Document, d_function.Error, d_in.Text, Parameters>
+
+//depencencies
+import * as t_from_loc from "./list_of_characters"
+
+export const Document: Document = ($, abort, $p) => {
+    return {
+        'content': Value($, abort, $p)
+    }
+}
+
+export const Value: Value = ($, abort, $p) => t_from_loc.Value(
+    _p_list_from_text(
+        $,
+        ($) => $
+    ),
+    abort,
+    $p
+)
