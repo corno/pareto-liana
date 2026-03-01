c527fe96f4ff5aec8be3d3aefdd489a03f4fd80a
Corno Schraverus <corno@schraverus.com>
2026-03-01 15:25:18 +0100

    pdt: published version 0.0.22

--- pub/src/implementation/manual/transformers/get_unmarshalled_document/fountain_pen.ts
+++ pub/src/implementation/manual/transformers/get_unmarshalled_document/fountain_pen.ts
@@ -1,8 +1,7 @@
 import * as _p from 'pareto-core/dist/assign'
 import * as _pi from 'pareto-core/dist/interface'
 
-//data types
-import * as d_in from "../../../../interface/to_be_generated/get_unmarshalled_file"
+import * as d_in from "../../../../interface/to_be_generated/get_unmarshalled_document"
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
 import * as d_function_loc from "astn-core/dist/interface/to_be_generated/location_to_fountain_pen"
 
@@ -10,18 +9,18 @@ export namespace signatures {
     export type Error = _pi.Transformer_With_Parameter<d_in.Error, d_out.Phrase, d_function_loc.Parameters>
 }
 
-//dependencies
-import * as t_read_file_to_fountain_pen from "pareto-resources/dist/implementation/manual/transformers/read_file/fountain_pen"
-import * as t_load_pareto_document from "../get_unmarshalled_document/fountain_pen"
-
-//shorthands
 import * as sh from "pareto-fountain-pen/dist/shorthands/prose"
 
+//dependencies
+import * as t_ur_from_loc_to_fp from "../unmarshall_result_from_loc/fountain_pen"
 
 export const Error: signatures.Error = ($, $p) => _p.decide.state($, ($) => {
     switch ($[0]) {
-        case 'document': return _p.ss($, ($) => t_load_pareto_document.Error($, $p))
-        case 'read file': return _p.ss($, ($) => t_read_file_to_fountain_pen.Error($))
+        case 'no schema file': return _p.ss($, ($) => sh.ph.composed([
+            sh.ph.literal("no schema file @ "),
+            sh.ph.literal($['file location'])
+        ]))
+        case 'deserialize': return _p.ss($, ($) => t_ur_from_loc_to_fp.Error($, $p))
         default: return _p.au($[0])
     }
 })
--- pub/src/implementation/manual/transformers/unmarshall_result_from_loc/fountain_pen.ts
+++ pub/src/implementation/manual/transformers/unmarshall_result_from_loc/fountain_pen.ts
@@ -1,7 +1,7 @@
 import * as _p from 'pareto-core/dist/assign'
 import * as _pi from 'pareto-core/dist/interface'
 
-import * as d_in from "../../../../interface/to_be_generated/get_unmarshalled_document"
+import * as d_in from "../../../../interface/to_be_generated/unmarshall_result_from_loc"
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
 import * as d_function_loc from "astn-core/dist/interface/to_be_generated/location_to_fountain_pen"
 
@@ -17,24 +17,14 @@ import * as t_deserialize_resolved_to_fountain_pen from "liana-core/dist/impleme
 
 export const Error: signatures.Error = ($, $p) => _p.decide.state($, ($) => {
     switch ($[0]) {
-        case 'no schema file': return _p.ss($, ($) => sh.ph.composed([
-            sh.ph.literal("no schema file @ "),
-            sh.ph.literal($['file location'])
-        ]))
-        case 'deserialize': return _p.ss($, ($) => _p.decide.state($, ($) => {
-            switch ($[0]) {
-
-                case 'schema': return _p.ss($, ($) => {
-                    return sh.ph.composed([
-                        sh.ph.literal("error in schema"),
-                        sh.ph.literal(": "),
-                        t_deserialize_resolved_to_fountain_pen.Error($.error, $p)
-                    ])
-                })
-                case 'parse': return _p.ss($, ($) => t_deserialize_parse_tree_to_fountain_pen.Error($, $p))
-                default: return _p.au($[0])
-            }
-        }))
+        case 'schema': return _p.ss($, ($) => {
+            return sh.ph.composed([
+                sh.ph.literal("error in schema"),
+                sh.ph.literal(": "),
+                t_deserialize_resolved_to_fountain_pen.Error($.error, $p)
+            ])
+        })
+        case 'parse': return _p.ss($, ($) => t_deserialize_parse_tree_to_fountain_pen.Error($, $p))
         default: return _p.au($[0])
     }
 })
