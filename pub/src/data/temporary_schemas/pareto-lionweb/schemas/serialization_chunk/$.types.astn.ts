5072b9d0223a7580f898780b0fc7bbd37cd0aced
Corno Schraverus <corno@schraverus.com>
2026-03-07 09:50:39 +0100

    .

--- pub/src/data/temporary_schemas/pareto-lionweb/schemas/serialization_chunk/$.types.astn.ts
+++ pub/src/data/temporary_schemas/pareto-lionweb/schemas/serialization_chunk/$.types.astn.ts
@@ -13,8 +13,6 @@ import * as g_ from "../../../../../interface/generated/liana/schemas/schema/dat
 
 export const $: g_.Modules = modules(
     {
-
-        "Serialization Chunks": module_(t.dictionary(t.component("Serialization Chunk"))),
         
         "Serialization Chunk": module_(t.group({
             "serializationFormatVersion": prop(t.text_local(text('single line'))),
