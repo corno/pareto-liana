b62ce9dadcb5148ea56d4779e7103ffe4ec179d3
Corno Schraverus <corno@schraverus.com>
2026-03-11 20:06:31 +0100

    .

--- pub/src/modules/common_tool_signatures/interface/to_be_generated/transform_file.ts
+++
@@ -1,7 +0,0 @@
-import * as d_file_in_file_out from "./file_in_file_out"
-
-import * as d_fp from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
-
-export type Error =
-    | ['processing', d_fp.Phrase]
-    | ['file in file out', d_file_in_file_out.Command_Error]
