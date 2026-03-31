26f45f9b99fa10fb74aeec18ffeb4ef7318858a8
Corno Schraverus <corno@schraverus.com>
2026-03-31 08:57:16 +0200

    pdt: published version 0.1.13

--- pub/src/interface/generated/liana/schemas/load_document/data.ts
+++
@@ -1,6 +0,0 @@
-
-import * as _pi from 'pareto-core/dist/interface'
-
-import * as i_imports_location from "../location/data"
-
-export { }
--- pub/src/interface/generated/liana/schemas/load_document/signatures/transformers/fountain_pen.ts
+++
@@ -1,8 +0,0 @@
-
-import * as _pi from 'pareto-core/dist/interface'
-
-import * as i_in from "../../data"
-
-import * as i_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
-
-export { }
