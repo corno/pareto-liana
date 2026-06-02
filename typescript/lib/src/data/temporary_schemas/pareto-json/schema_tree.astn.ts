25a65efcfedf87a6d07e6b123b8ba95513cf8da3
Corno Schraverus <corno@schraverus.com>
2026-06-02 12:03:25 +0200

    pdt: ...

--- typescript/lib/src/data/temporary_schemas/pareto-json/schema_tree.astn.ts
+++ typescript/lib/src/data/temporary_schemas/pareto-json/schema_tree.astn.ts
@@ -2,11 +2,10 @@ import * as _pi from 'pareto-core/dist/interface'
 
 import * as sh from "../../../shorthands/schema"
 
-
-import { $ as x_json } from "./schemas/json/$.module.astn"
-import { $ as x_json_with_unique_keys } from "./schemas/json_with_unique_keys/$.module.astn"
+import { $ as x_json_with_guaranteed_unique_keys } from "./schemas/json_with_guaranteed_unique_keys/$.module.astn"
+import { $ as x_json_without_guaranteed_unique_keys } from "./schemas/json_without_guaranteed_unique_keys/$.module.astn"
 
 export const $ = sh.st.set({
-    "json": x_json,
-    "json with unique keys": x_json_with_unique_keys,
+    "json with guaranteed unique keys": x_json_with_guaranteed_unique_keys,
+    "json without guaranteed unique keys": x_json_without_guaranteed_unique_keys,
 })
