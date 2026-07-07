3802fc1bad071908dc1a0b8f2f1034c80ba012a5
Corno Schraverus <corno@schraverus.com>
2026-07-07 15:29:07 +0200

    pdt: ...

--- typescript/app/src/bin/serialize_schemas.ts
+++ typescript/app/src/bin/serialize_schemas.ts
@@ -2,7 +2,7 @@
 
 import * as p_h from 'pareto-host-nodejs/index'
 
-import * as rs_filesystem_unrestricted from "pareto-host-nodejs/file_system_unrestricted/index"
+import * as rs_filesystem_unrestricted from "pareto-resource-filesystem-unrestricted/index"
 import * as rs_stream from "pareto-resource-stream/index"
 
 import { $$ as procedure } from "lib/implementation/manual/commands/serialize_schemas"
