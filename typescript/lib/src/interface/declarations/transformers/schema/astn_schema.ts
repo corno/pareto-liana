c0b356bc840c4c72a8b9cbbc6d7f5b2a146e3aca
Corno Schraverus <corno@schraverus.com>
2026-07-08 13:44:15 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/schema/astn_schema.ts
+++ typescript/lib/src/interface/declarations/transformers/schema/astn_schema.ts
@@ -1,6 +1,5 @@
 
 import type * as p_ from 'pareto-core/interface/transformer'
-import p_unreachable_code_path from 'pareto-core/implementation/transformer/specials/unreachable_code_path'
 
 //data types
 import type * as d_in from "../../../generated/liana/schemas/schema/data/resolved.js"
