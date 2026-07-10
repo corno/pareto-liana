f2e950dbaf23b49b2bc1a8f5f24aba653efb29fe
Corno Schraverus <corno@schraverus.com>
2026-07-10 10:21:04 +0200

    pdt: ...

--- typescript/lib/src/declarations/transformers/schema/astn_schema.ts
+++ typescript/lib/src/declarations/transformers/schema/astn_schema.ts
@@ -1,33 +1,33 @@
 
-import type * as p_ from 'pareto-core/interface/transformer'
+// import type * as p_ from 'pareto-core/interface/transformer'
 
-//data types
-import type * as d_in from "../../../modules/schema/interface/data/resolved.js"
-import type * as d_out from "../../../interface/generated/liana/schemas/astn_schema/data/unresolved.js"
+// //data types
+// import type * as d_in from "../../../modules/schema/interface/data/resolved.js"
+// import type * as d_out from "../../../interface/generated/liana/schemas/astn_schema/data/unresolved.js"
 
 
-export type Schema = p_.Transformer<
-    d_in.Schema,
-    d_out.Schema
->
-export type Globals = p_.Transformer<
-    d_in.Globals,
-    d_out.Globals
->
-export type Schema_Imports = p_.Transformer<
-    d_in.Schema_Imports,
-    d_out.Imports
->
-export type Module = p_.Transformer<
-    d_in.Module,
-    d_out.Modules.l_dictionary.D.l_entry
->
-export type Value = p_.Transformer<
-    d_in.Value,
-    d_out.Value
->
-export type Text_Type = p_.Transformer<
-    d_in.Text_Type,
-    d_out.Text_Type
->
+// export type Schema = p_.Transformer<
+//     d_in.Schema,
+//     d_out.Schema
+// >
+// export type Globals = p_.Transformer<
+//     d_in.Globals,
+//     d_out.Globals
+// >
+// export type Schema_Imports = p_.Transformer<
+//     d_in.Schema_Imports,
+//     d_out.Imports
+// >
+// export type Module = p_.Transformer<
+//     d_in.Module,
+//     d_out.Modules.l_dictionary.D.l_entry
+// >
+// export type Value = p_.Transformer<
+//     d_in.Value,
+//     d_out.Value
+// >
+// export type Text_Type = p_.Transformer<
+//     d_in.Text_Type,
+//     d_out.Text_Type
+// >
 
